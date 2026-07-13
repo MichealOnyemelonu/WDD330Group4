38646722d88123c42aa1a030c95518eaaac2eaf7
MichealOnyemelonu <monyemelonu@gmail.com>
2026-07-14 00:30:41 +0200

    updated the .js file of cart directory to have updatedCartCount() and have a call feature as well

--- src/js/cart.js
+++ src/js/cart.js
@@ -25,4 +25,15 @@ function cartItemTemplate(item) {
   return newItem;
 }
 
+
+function updateCartCount() {
+  const cartItems = getLocalStorage("so-cart");
+  const count = cartItems.length;
+  const countBadge = document.querySelector(".cart__count");
+  countBadge.textContent = count;
+}
+
+
 renderCartContents();
+
+updateCartCount();
