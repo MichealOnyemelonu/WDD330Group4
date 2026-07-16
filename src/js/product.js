c45b84d946b445389402a330c58f1d25830d57bc
Richard Edigin <richardedigin7988@gmail.com>
2026-07-16 08:49:53 -0500

    corrected errors

--- src/js/product.js
+++ src/js/product.js
@@ -5,27 +5,18 @@ import ProductDetails from "./productDetails.mjs";
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> c0d2c5524bade7ab9ca1c5ad90f6e519a1b5d5cb
 const product = new ProductDetails(productID, dataSource);
 product.init();
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-<<<<<<< HEAD
-  const selectProduct = await dataSource.findProductById(e.target.dataset.id);
-
-  addProductToCart(selectProduct);
-=======
   const selectProduct = 
     await dataSource.findProductById(e.target.dataset.id);
   
   addProductToCart(product); 
->>>>>>> c0d2c5524bade7ab9ca1c5ad90f6e519a1b5d5cb
 }
 
 // add listener to Add to Cart button
