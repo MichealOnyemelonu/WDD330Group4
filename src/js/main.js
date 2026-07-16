2e310b363b214f4f440664c0436584b45f51334f
Richard Edigin <richardedigin7988@gmail.com>
2026-07-16 11:21:53 -0500

    i added a customisable alert to the product listing page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.js";
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
@@ -7,3 +8,11 @@ const listElement = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, listElement);
 
 await productList.init();
+
+// default: fetches '/json/alerts.json' and appends <section class="alert-list"> to document.body
+const alerts = new Alert();
+await alerts.loadAndRender();
+
+// OR: render into a specific container in your page
+// const alerts = new Alert({ container: '#alerts-container' });
+// await alerts.loadAndRender();
