c446afd6fd7696980b62d1f5b4786ebe29dc96f1
Richard Edigin <richardedigin7988@gmail.com>
2026-07-18 18:05:16 +0100

    i corrected bugs that existed in my work, which was adding a customisable alert to the site

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,7 @@ const productList = new ProductList("tents", dataSource, listElement);
 
 await productList.init();
 
-// default: fetches '/json/alerts.json' and appends <section class="alert-list"> to document.body
+
 const alerts = new Alert();
 await alerts.loadAndRender();
 
-// OR: render into a specific container in your page
-// const alerts = new Alert({ container: '#alerts-container' });
-// await alerts.loadAndRender();
